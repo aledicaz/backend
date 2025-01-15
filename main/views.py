c44792b7f101bf0eb5de817f6e4f1214ae6004fc
Alex <licazago@fiec.espol.edu.ec>
2025-01-15 09:45:28 -0500

    Guia 24

--- main/views.py
+++ main/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+ # Importe el decorador login_required
+from django.contrib.auth.decorators import login_required, permission_required
+
  # Importe requests y json
 import requests
 import json
 from collections import Counter
 from datetime import datetime
 
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
 def index(request):
         
      # Arme el endpoint del REST API
